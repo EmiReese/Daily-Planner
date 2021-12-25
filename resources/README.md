7f034c261afcb56b60cae47dec29eeebfac85de1
Emily Reese <emilyreese.ga@gmail.com>
2021-12-24 23:24:44 -0500

    Inital commit for this project. Right now, this project is based off HTML and CSS. In the future, it will contain javascript and jQuery.

---
+++ resources/README.md
@@ -0,0 +1,3 @@
+# Daily-Planner
+
+I have created a project using CSS, HTML, iQuery and JS. I am creating a daily planner that will tell the time, and be color coded depending on the past, present and future. 
---
+++ script.js
@@ -0,0 +1,75 @@
+
+var rows = $('#row');
+var targetEl =$('#target');
+var addBtnEl =$('#addBtn');
+var formEl = $('form');
+var containerEl = $('container')
+
+
+
+
+// HELPFUL NOTES :
+
+// This could be helpful for the time problem
+// jQuery.when()
+// Provides a way to execute callback functions based on zero or more Thenable objects, usually Deferred objects that represent asynchronous events. 
+
+// This could be helpful storing our data
+// jQuery.data()
+// Store arbitrary data associated with the specified element and/or return the value that was set.
+
+// jQuery.parseJSON()
+// Takes a well-formed JSON string and returns the resulting JavaScript value.
+
+// .fadeIn()
+// Display the matched elements by fading them to opaque.
+
+// .fadeOut()
+// Hide the matched elements by fading them to transparent.
+
+
+
+
+
+
+//This part of my Jquery/JS is supposed to control the users input (event/appointment) for that hour.
+// var containerEl = [{
+//      {
+//           hour: '9 am',
+//           userInput:  ' ',
+//           },
+//      {
+//           hour: '10 am',
+//           userInput:  ' ',
+//      },
+//       {
+//           hour: '11 am',
+//           userInput:  ' ',
+//       },
+//       {
+//           hour: '12 pm',
+//           userInput:  ' ',
+//       },
+//       {
+//           hour: '1 pm',
+//           userInput:  ' ',
+//       },
+//       {
+//           hour: '2pm',
+//           userInput:  ' ',
+//       },
+//      {
+//           hour: '3 pm',
+//           userInput:  ' ',
+//      },
+//      {
+//           hour: '4 pm',
+//           userInput:  ' ',
+//      },
+//      {
+//           hour: '5 pm',
+//           userInput:  ' ',
+//      },
+// }
+// }]; 
+
