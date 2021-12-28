231b0cc9dc8d23b5b7a7ced44ce34a00bfc1ad65
Emily Reese <emilyreese.ga@gmail.com>
2021-12-27 19:15:52 -0500

    I have added a working clock in my Javascript in the header of the page.

--- script.js
+++ script.js
@@ -15,6 +15,53 @@
 //         this.appt = appt;
 //     }
 // }
+function showTime(){
+  // DATE SECTION
+    var myDate = new Date();
+    var year = myDate.getFullYear();
+    if (year < 1000){
+      year += 1900
+    }
+    var day = myDate.getDay();
+    var month = myDate.getMonth();
+    var dayM = myDate.getDate();
+    var dayArray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",  "Saturday");
+    var monthArray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    
+    // TIME SECTION
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var m= currentTime.getMinutes();
+    var s= currentTime.getSeconds();
+    if (h === 24){
+      h = 0;
+    } else if (h > 12){
+      h = h - 0;
+    }
+    if (h < 10) {
+      h = "0" + h;
+    } 
+    if (m < 10) {
+      m = "0" + m;
+    }
+    if (s < 10){
+      s = "0" + s;
+    }
+// DISPLAY FOR DATE AND TIME 
+    var myClock = document.getElementById('currentDay');
+    myClock.textContent = "" + dayArray[day] + " " + dayM + " " + monthArray[month] +
+ " " + year + " | " + h + ":" + m +  ":" + s;
+ myClock.innerText = "" + dayArray[day] + " " + dayM + " " + monthArray[month] +
+ " " + year + " | " + h + ":" + m +  ":" + s;
+
+ setTimeout("showTime()", 1000);  }
+
+// CALL FUNCTION
+ showTime();
+
+
+
+
 //UI class
 class UI {
 static displayAppt () {
